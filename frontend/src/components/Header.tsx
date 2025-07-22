13cbfa268a144c5563bdc755253363d8b2de9fb0
mrstsgk <s.m.0819aaa@gmail.com>
2025-07-22 22:40:07 +0900

    feat: ヘッダーコンポーネントを追加
    ユーザー名とログアウトボタンを含むヘッダーを作成し、スタイルを適用した

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import styles from "./Header.module.scss";
+
+interface HeaderProps {
+  userName: string;
+  onLogout: () => void;
+}
+
+const Header: FC<HeaderProps> = ({ userName, onLogout }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
+          <img
+            src="/images/suma_touen_image_logo.png"
+            alt="スマ登園ロゴ"
+            className={styles.logoImage}
+          />
+          <span className={styles.logoText}>スマ登園</span>
+        </div>
+        <div className={styles.userSection}>
+          <span className={styles.userName}>{userName}さん</span>
+          <button className={styles.logoutButton} onClick={onLogout}>
+            ログアウト
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
