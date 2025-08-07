413e3f18305043953972948ae653c8011f0c41a8
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-07 17:15:18 +0900

    chore: Reactと関連パッケージのバージョンを更新
    Reactとreact-domを19.1.1に、@types/reactと@types/react-domをそれぞれ19.1.9と19.1.7に更新し、babel-plugin-react-compilerを追加した

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
   server: {
+    host: "0.0.0.0",
     port: 3000,
-    open: true,
     proxy: {
       "/api": {
         target: "http://localhost:8080",
