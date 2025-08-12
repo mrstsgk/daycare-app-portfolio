40d8f55ec3f258771ed5549dfbbecd72c43429be
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-12 22:47:29 +0900

    feat: OpenAPIの自動生成

---
+++ backend/presentation/src/main/kotlin/com/example/daycare/presentation/model/BadRequestErrorResponseModel.kt
@@ -0,0 +1,26 @@
+package com.example.daycare.presentation.model
+
+import com.example.daycare.presentation.model.ApiErrorModel
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+import jakarta.validation.Valid
+
+/**
+ * 
+ * @param error 
+ */
+data class BadRequestErrorResponseModel(
+
+    @field:Valid
+    @get:JsonProperty("error") val error: kotlin.collections.List<ApiErrorModel>? = null
+    ) {
+
+}
+
---
+++ backend/presentation/src/main/kotlin/com/example/daycare/presentation/model/ClassRoomListResponseModel.kt
@@ -0,0 +1,27 @@
+package com.example.daycare.presentation.model
+
+import com.example.daycare.presentation.model.ClassRoomModel
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+import jakarta.validation.Valid
+
+/**
+ * クラス情報一覧取得レスポンス
+ * @param classRooms クラス情報の配列
+ */
+data class ClassRoomListResponseModel(
+
+    @field:Valid
+    @field:NotNull
+    @get:JsonProperty("classRooms") val classRooms: kotlin.collections.List<ClassRoomModel>
+    ) {
+
+}
+
